81ab5a49c5d0479716a3ee09b1c2dc03999d0450
Andrius Kairys <andrius.kairys.lt@gmail.com>
2020-11-26 23:14:13 +0200

    pre form

--- main.js
+++ main.js
@@ -14,8 +14,9 @@ console.log(op); */
 function skinChange() {
     let x = document.querySelector("#skin > input:checked").value;
     let y = document.querySelector("#hairLength > input:checked").value;
+    let z = document.querySelector("#beard > input:checked").value;
     if (y !== 'bald') {
-        document.querySelector('div.face').className = `face ${x}`;
+        document.querySelector('div.face').className = `face ${x} ${z}`;
     } else {
         document.querySelector('div.face').className = `face ${x}`;
         document.querySelector('div.hair').className = `hair ${x}`;
@@ -48,6 +49,33 @@ function eyesColor() {
     document.querySelector("div.eyes").className = `eyes ${x}`;
     document.querySelector("div.eyes:nth-child(2)").className = `eyes ${x}`;
 }
+
+function beardColor() {
+    let x = document.querySelector("#skin > input:checked").value;
+    let z = document.querySelector("#beard > input:checked").value;
+        document.querySelector('div.face').className = `face ${x}  ${z}`;
+  }
+
+ function nosePic() {
+    let x = document.querySelector("div.nose");
+    let z = document.querySelector("#nose > input:checked").value;
+    if (z === 'nonose') {
+         x.innerHTML = '';
+         x.className = `nose`;
+    } else if (z === 'nose1') {
+         x.innerHTML = '<img src="/nose1.png" alt="nose1"></img>';
+         x.className = `nose`;
+    } else if (z === 'nose2') {
+        x.innerHTML = '<img src="/nose2.png" alt="nose2"></img>';
+    } else if ( z === 'mask') {
+        x.innerHTML = '<img src="/mask.png" alt="mask"></img>';
+        x.className = `nose mask`;
+    } else {
+        x.innerHTML = '';
+        x.className = `nose`;
+    }
+     
+ } 
 
 
 
