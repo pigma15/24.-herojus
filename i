bfd5ceb6dd0e25e59cdae2f1ec4d9d0837e7a817
Andrius Kairys <andrius.kairys.lt@gmail.com>
2020-11-26 22:05:38 +0200

    eyes done

---
+++ i
@@ -0,0 +1,5 @@
+
+
+blond,brown,black,grey,cyber
+blue,!brown,!grey,
+no,!blond, !!brown, !!black, !grey, !blue
--- main.js
+++ main.js
@@ -10,12 +10,44 @@ function myFunction() {
 /* const op = document.querySelector('#skin > input:checked').value;
 console.log(op); */
 
-const jop = document.querySelector('div.face');
-console.log(jop);
 
-function myFunction() {
+function skinChange() {
     let x = document.querySelector("#skin > input:checked").value;
-    document.querySelector('div.face').className = `face ${x}`;
+    let y = document.querySelector("#hairLength > input:checked").value;
+    if (y !== 'bald') {
+        document.querySelector('div.face').className = `face ${x}`;
+    } else {
+        document.querySelector('div.face').className = `face ${x}`;
+        document.querySelector('div.hair').className = `hair ${x}`;
+    }
   }
+
+function hairLengthF() {
+    let y = document.querySelector("#hairLength > input:checked").value;
+    let x = document.querySelector("#skin > input:checked").value;
+    if (y !== 'bald') {
+        document.querySelector('div.hair').className = `hair ${y}`;
+    } else {
+        document.querySelector('div.hair').className = `hair ${x}`;
+    }
+  }
+
+function hairColorF() {
+    let y = document.querySelector("#hairLength > input:checked").value;
+    let z = document.querySelector("#hairColor > input:checked").value;
+    let x = document.querySelector("#skin > input:checked").value;
+    if (y !== 'bald') {
+        document.querySelector('div.hair').className = `hair ${y} ${z}`;
+    } else {
+        document.querySelector('div.hair').className = `hair ${x}`;
+    }
+  }
+
+function eyesColor() {
+    let x = document.querySelector("#eyes > input:checked").value;
+    document.querySelector("div.eyes").className = `eyes ${x}`;
+    document.querySelector("div.eyes:nth-child(2)").className = `eyes ${x}`;
+}
+
 
 
